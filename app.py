ef85f20ea9c34011f796a6d6bbe6a8cc8fc608be
Aleksei_Iliushchenko <Aleksei_Iliushchenko@epam.com>
2022-05-24 18:33:43 +0300

    almost_final

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import psycopg
 from func_Days_less10 import func_Days_less10
 from func_for_days_more_than_10 import func_for_days_more_than_10
 from models import cursor
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 models.create_database_and_table()
 func_Days_less10()
 func_for_days_more_than_10()
-
 
 
 conn = psycopg.connect(
@@ -16,31 +15,31 @@ conn = psycopg.connect(
 conn.autocommit = True
 
 app = Flask (__name__)
-@app.route("/")
+
+@app.route("/",  methods=['POST', 'GET'])
+def spisok():
+  get_data_from_db = "SELECT DISTINCT data from currencies ORDER by data;";
+  cursor.execute(get_data_from_db)
+  data=cursor.fetchall()
+  return render_template('spisok.html', data=data)
+
+
+
+
+@app.route("/currencies", methods=['POST', 'GET'])
 def home_page():
-  get_data_from_db = "SELECT * FROM currencies";  #'SELECT * FROM currencies ORDER BY data, name';
+  chosen_data = request.form.get('data')
+  get_data_from_db = f"select * from currencies  where data = '{chosen_data}' order by name;";
   cursor.execute(get_data_from_db)
   my_table=cursor.fetchall()
-  for row in my_table:
-      data=row[0]
-      id=row[1]
-      numcode=row[2]
-      charcode=row[3]
-      nominal=row[4]
-      name=row[5]
-      value=row[6]
-      #print( id, numcode, charcode, nominal, name, value )
-      return render_template('main.html', data=data, id=id, numcode=numcode, charcode=charcode, nominal=nominal, name=name, value=value)
+    # cursor.close()
+    # conn.close()
+
+  return render_template('main.html', my_table=my_table, chosen_data=chosen_data)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 1245)
-
-
-
-
-
-
-
 
 
 
